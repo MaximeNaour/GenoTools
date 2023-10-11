b02060a6c7168a20596fb9a2203b653d54194318
Maxime Naour <naour.maxime@gmail.com>
2023-10-11 17:03:56 +0200

    Update blast-genome.sh

--- Scripts/blast-genome.sh
+++ Scripts/blast-genome.sh
@@ -3,7 +3,7 @@
 # Author: Maxime Naour
 # Date(dd-mm-yyyy): 11-10-2023
 
-# Disclaimer : Activer l'environnement conda contenant les packages edirect, fastx_toolkit et blast ~~> Pour moi, ces packages sont dans cet environnement conda : conda activate edirect 
+# Disclaimer : Activer les environnements conda edirect et ncbi : conda activate edirect ncbi
 
 # Vérifier si l'environnement conda edirect est activé
 if [[ $(conda info --envs | grep -c "^\s*edirect\s") -eq 0 ]]; then
@@ -92,8 +92,69 @@ fi
 read -p "Voulez-vous réaliser le BLAST ? (y/n) " run_blast
 if [ "$run_blast" = "y" ]; then
   echo "Réalisation du BLAST en cours..."
-  blastn -query ${fastq_file%.*}.fna -db temp/${filename%.*.gz} -out output.txt -num_threads 6
+  blastn -query ${fastq_file%.*}.fna -db temp/${filename%.*.gz} -out output.txt -num_threads 6 
+
+  # Modifier le fichier de sortie pour afficher les résultats pour chaque séquence
+  echo "Modification du fichier de sortie en cours..."
+  sequence_count=1
+  hit_count=0
+  no_hit_count=0
+  while IFS= read -r line; do
+      if [[ $line == "Query="* ]]; then
+          echo "*****************************"
+          echo "Result sequence $sequence_count"
+          echo "*****************************"
+          echo "$line"
+          ((sequence_count++))
+      elif [[ $line == "***** No hits found *****" ]]; then
+          echo "$line"
+          ((no_hit_count++))
+      elif [[ $line == "Sequences producing significant alignments:"* ]]; then
+          echo "$line"
+          ((hit_count++))
+      else
+          echo "$line"
+      fi
+  done < "output.txt" > output_space.txt
+
+  # Ajouter les informations générales du BLAST en haut du fichier
+  echo "~ Summary of BLAST ~" > temp_output.txt
+  echo "Total number of blasted sequences : $((sequence_count-1))" >> temp_output.txt
+  echo "Number of sequences with a hit : $hit_count" >> temp_output.txt
+  echo "Number of sequences without hits : $no_hit_count" >> temp_output.txt
+  echo "~ ~ ~ ~ ~ ~ ~ ~ ~ ~" >> temp_output.txt
+  echo "" >> temp_output.txt
+  cat output_space.txt >> temp_output.txt
+  mv temp_output.txt processed_${fastq_file%.*}.txt && echo "Le fichier de sortie a été renommé en processed_${fastq_file%.*}.txt"
+  # Supprimer les fichiers output.txt et output_space.txt
+  rm output.txt output_space.txt
 fi
 
-# Renommer le fichier de sortie en processed_$fastq_file.txt
-mv output.txt processed_${fastq_file%.*}.txt && echo "Le fichier de sortie a été renommé en processed_${fastq_file%.*}.txt"
+# Définir le nom du fichier
+input_file="processed_${fastq_file%.*}.txt"
+
+# Extraire le bloc de texte à déplacer
+block_to_move=$(awk '/~ Summary of BLAST ~/,/~ ~ ~ ~ ~ ~ ~ ~ ~ ~/ {print}' "$input_file")
+
+# Supprimer les six premières lignes du fichier
+sed -i '1,6d' "$input_file"
+
+# Définir un motif partiel
+partial_pattern="Result sequence 1"
+
+# Trouver la ligne correspondant au motif partiel
+line_number=$(grep -n "$partial_pattern" "$input_file" | cut -d: -f1)
+
+# Vérifier si la ligne a été trouvée avant d'effectuer l'insertion
+if [ -n "$line_number" ]; then
+    # Calculer le numéro de la ligne où insérer le bloc
+    insert_line=$((line_number - 2))
+    
+    # Insérer le bloc de texte deux lignes avant la ligne trouvée
+    awk -v block="$block_to_move" -v line="$insert_line" 'NR==line {print block} {print}' "$input_file" > temp_file.txt && mv temp_file.txt "$input_file"
+else
+    echo "Pattern non trouvé dans le fichier."
+fi
+
+echo "Le processus est terminé."
+
